bd4ed16a7654874c9a6b05882d166053491cedbd
MarcinMorgan <morgangzit@gmail.com>
2024-09-04 23:15:31 +0200

    t1 done

--- 01/app.js
+++ 01/app.js
@@ -4,13 +4,38 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class App extends React.Component {
-    state = {
-        counter: 0,
+    constructor(props) {
+        console.log('constructor')
+
+        super(props);
+        this.state = {
+            counter: 0,
+        }
     }
     render() {
         console.log('render');
 
         return <h1>{ this.state.counter }</h1>
+    }
+    componentDidMount() {
+        console.log('componentDidMount');
+
+        this.id = setInterval(() => {
+            const {counter} = this.state;
+            this.setState( {counter: counter +1})
+        }, 5000);
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+
+        if (this.state.counter == 1) {
+            this.componentWillUnmount();
+        };
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+
+        clearInterval(this.id)
     }
 }
 
