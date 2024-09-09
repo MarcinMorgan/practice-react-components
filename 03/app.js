7a282d8b64dd6c05924c5891f7684237d6dfd477
MarcinMorgan <morgangzit@gmail.com>
2024-09-09 17:25:51 +0200

    t3 done, fix textarea not clearing after adding comment

--- 03/app.js
+++ 03/app.js
@@ -9,29 +9,61 @@ class Article extends React.Component {
     }
     
     render() {
-        const {title, body} = this.props;
+        const {title, body, content} = this.props;
         return (
             <article>
                 <h1>{ title }</h1>
                 <p>{ body }</p>
                 <section>
-                    <form>
+                    <form onSubmit={ this.submitHandler }>
                         <div>
                             <label>
                                 <textarea 
                                     style={{ "minWidth": "300px", "minHeight": "120px" }} 
-                                    name="content" 
+                                    name="content"
+                                    onChange={ this.changeHandler }
                                 />
                             </label>
                         </div>
-                        <div><input type="submit" value="dodaj komentarz" /></div>
+                        <div><input type="submit" value="dodaj komentarz"/></div>
                     </form>
                     <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        { this.renderCommentList() }
                     </ul>
                 </section>
             </article>
         )
+    }
+
+    changeHandler = e => {
+        const {name, value} = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    submitHandler = e => {
+        e.preventDefault()
+        const comment = this.state.content
+        this.addComment(comment);
+        this.setState({
+            content: ''
+        })
+    }
+
+    addComment(newComment) {
+        this.setState({
+            comments: [...this.state.comments, newComment],
+        });
+    }
+
+    renderCommentList() {
+        const comments = this.state.comments;
+        return comments.map( comment => { 
+            return (
+                <li>{ comment }</li>
+            )
+        })
     }
 }
 
